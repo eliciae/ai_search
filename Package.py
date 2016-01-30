283bcd0a8f1abac3561455c3b1062be17bc38c75
pey849 <peteryeung79@gmail.com>
2016-01-30 14:10:22 -0600

    added get methods for node retrieval

--- Package.py
+++ Package.py
@@ -6,5 +6,11 @@ import random as rand
 class Package:
 
     def __init__(self, startLocation, endLocation):
-        self.source = startLocation
-        self.destination = endLocation
+        self.startLocation = startLocation
+        self.endLocation = endLocation
+
+    def getNodeStartLocation(self):
+        return self.startLocation
+
+    def getNodeEndLocation(self):
+        return self.endLocation
--- Vehicle.py
+++ Vehicle.py
@@ -1,7 +1,7 @@
-from ai_search import Package
-
 class Vehicle:
     HomeLocation = 0
 
     def __init__(self):
         self.myLoadOfPackages = []
+
+
