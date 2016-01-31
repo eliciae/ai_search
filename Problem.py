1dafa05404e8ee7008003eed4b94b79ab8c8c6a0
pey849 <peteryeung79@gmail.com>
2016-01-31 13:09:51 -0600

    Changes some things in Problem.py and State.py

--- Problem.py
+++ Problem.py
@@ -1,15 +1,29 @@
 from ai_search import Vehicle as truck
+from ai_search import State
+import networkx as nx
+
 
 class Problem:
+    graph = None
 
-    def __init__(self, map, listOfPackages, listOfVehicles):
-        self.map = map
+    def __init__(self, myMap, listOfPackages, listOfVehicles):
+        Problem.graph = myMap
         self.listOfPackages = listOfPackages
         self.listOfVehicles = listOfVehicles
 
     def isGoal(self, state):
         return not self.listOfPackages and not self.listOfVehicles
 
-
     def successors(self, state):
+        # getNeighbours(current Node) curr node comes from the
+        # vehicle which comes from a state
+        vehicles = State.getVehicleList()
+        for v in vehicles:
+            neighbours = Problem.graph.neighbors(v.getCurrLocation())
+            states = list()
+            for n in neighbours:
+                updatedVehicles = truck.Vehicle()
+
+        # make node into state list from given state
+        # returns a node list
         return None
--- State.py
+++ State.py
@@ -1,10 +1,10 @@
 class State:
-    def __init__(self, myVehicle, myListOfPackages):
-        self.MyVehicle = myVehicle
-        self.MyListOfPackages = myListOfPackages
+    def __init__(self, vehicleList, packagePickUpList):
+        self.vehicleList = vehicleList
+        self.packagePickUpList = packagePickUpList
 
-    def getMyVehicle(self):
-        return self.MyVehicle
+    def getVehicleList(self):
+        return self.vehicleList
 
-    def getMyPackageList(self):
-        return self.MyListOfPackages
+    def getPackageList(self):
+        return self.packagePickUpList
