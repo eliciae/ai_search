c2d9cf10483d98c58acfd09552728332c40bb750
eliciae <elicia.erker@gmail.com>
2016-02-05 21:49:43 -0600

    added a priority queue

--- Search.py
+++ Search.py
@@ -6,7 +6,7 @@ class Search:
         return
 
     def search(self, problem, initialState):
-        searchQueue = queue.Queue()
+        searchQueue = queue.PriorityQueue()
         searchQueue.put(initialState)
         while(searchQueue.not_empty):
             here = searchQueue.get()
@@ -17,6 +17,6 @@ class Search:
                 print("Current Vehicle Location: {0}" .format(here.getVehicleList().getCurrLocation()))
                 nextState = problem.successors(here)
                 for s in nextState:
-                    searchQueue.put(s)
+                    searchQueue.put(len(s.getAStarList()), s)
                 #print(nextState.getVehicleList().getCurrLocation())
         return "Failed Hardcore"
--- State.py
+++ State.py
@@ -1,10 +1,17 @@
 class State:
-    def __init__(self, vehicleList, packagePickUpList):
+    def __init__(self, vehicleList, packagePickUpList, aStarList):
         self.vehicleList = vehicleList
         self.packagePickUpList = packagePickUpList
+        self.aStarList = aStarList
 
     def getVehicleList(self):
         return self.vehicleList
 
     def getPackageList(self):
         return self.packagePickUpList
+
+    def getAStarList(self):
+        return self.aStarList
+
+    def setAStarList(self, aStarList):
+        self.aStarList = aStarList
